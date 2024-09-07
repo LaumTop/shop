9e597b4f76ed8294d6745ad4c797bf08ade4625c
laumtop <dominiks052@gmail.com>
2024-09-07 15:11:11 +0500

    Started Contatcs.js and Contatcs.css Some fix for navbar.css, now novbar changes in other resolution

--- src/pages/Contacts/Contatcs.js
+++ src/pages/Contacts/Contatcs.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Navbar from '../Navbar/navbar';
+import './Contatcs.css';
 
-const Contatcs = () =>
-{
-    return(
-        <Navbar />
+const Contacts = () => {
+    return (
+        <>
+            <Navbar/>
+            <div className="Shape_Description">
+                <div className="Shape_Description_Title">
+                    Nepieciešama palīdzība?
+                </div>
+                <div className="Shape_Description_Description">
+                    Šajā lapā jūs varēstiet saņem palkīdzību no mūsu aģemtiem, izvēlieties Jums labāko variantu...
+                    Mēs mēģināsim jums atbildēt maksimāli atri, cik vien tas ir iespējams.
+                    Mūsu palīdzības centrs strādā <span style={{ fontWeight: 'bold' }}>katru dienu</span> no <span style={{fontWeight: 'bold'}}> 8.00 līdz 22.00</span> pēc latvijas pulkstens</div>
+            </div>
+        </>
     );
 };
 
-export default Contatcs;
+export default Contacts;
