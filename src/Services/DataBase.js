e284b8471cc28efc64a40079582630612701be8b
laumtop <dominiks052@gmail.com>
2024-09-07 17:23:27 +0500

    Download library for payment Created database for products and function for add product

--- src/Services/DataBase.js
+++ src/Services/DataBase.js
@@ -1,36 +1,67 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./Data.db', (err) => {
+const users = new sqlite3.Database('./Data.db', (err) => {
     if (err) {
-        console.error('DB error: ' + err.message);
+        console.error('Data | DB error: ' + err.message);
     } else {
-        console.log('Connected to the database');
+        console.log('Data | Connected to the database');
 
-        db.run(`CREATE TABLE IF NOT EXISTS user (
+        users.run(`CREATE TABLE IF NOT EXISTS user (
             ID INTEGER PRIMARY KEY,
             email TEXT NOT NULL, 
             product TEXT NOT NULL, 
             dateTime TEXT NOT NULL
         )`, (err) => {
             if (err) {
-                console.error('Table creation error: ' + err.message);
-            } else {
-                addNewUser("test@gmail.com", "telegram_bot");
+                console.error('Data | Table creation error: ' + err.message);
             }
         });
     }
 });
+const products = new sqlite3.Database('./Products.db', (err) =>
+{
+    if(err)
+    {
+        console.error('Products | DB error: ' + err.message);
+    } else
+    {
+        console.log('Products | Connected to the database');
+    }
+    products.run(`CREATE TABLE IF NOT EXISTS products (
+            ID INTEGER PRIMARY KEY,
+            Name TEXT NOT NULL, 
+            Cost INTEGER NOT NULL, 
+            Description TEXT NOT NULL
+        )`, (err) => {
+        if (err) {
+            console.error('Products | Table creation error: ' + err.message);
+        } else {
+        }
+    });
+});
+function addNewProduct(name, cost, description)
+{
 
+    products.run('INSERT INTO products (name, cost, description) VALUES (?, ?, ?)', [name, cost, description], function(err) {
+        if (err) {
+            console.error('Product | Error inserting data: ' + err.message);
+        }
+        users.close((err) => {
+            if (err) {
+                console.error('Product | Error closing the database: ' + err.message);
+            }
+        });
+    });}
 function addNewUser(email, product) {
     const dateTime = new Date().toISOString();
 
-    db.run('INSERT INTO user (email, product, dateTime) VALUES (?, ?, ?)', [email, product, dateTime], function(err) {
+    users.run('INSERT INTO user (email, product, dateTime) VALUES (?, ?, ?)', [email, product, dateTime], function(err) {
         if (err) {
-            console.error('Error inserting data: ' + err.message);
+            console.error('Users | Error inserting data: ' + err.message);
         }
-        db.close((err) => {
+        users.close((err) => {
             if (err) {
-                console.error('Error closing the database: ' + err.message);
+                console.error('Users | Error closing the database: ' + err.message);
             }
         });
     });
